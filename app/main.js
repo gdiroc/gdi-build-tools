dc06ee2b01c77cb20f6a41653bbfccef0bf3f517
Josh Kramer <jkjustjoshing@gmail.com>
2016-11-10 07:29:00 -0500

    Here's where the power begins:
    > npm install jquery --save 
    
    Require jquery and use it.
    
    Illustrate that jQuery isn't globally defined
    WARNING
    Chrome defines $ if it's not globally defined

--- app/main.js
+++ app/main.js
@@ -1,1 +1,10 @@
-console.log('Hello world');
+var $ = require('jquery');
+
+$('body').html('<h1>We have jquery!</h1>');
+
+// Is jQuery exposed globally or only locally?
+if (window.$ === undefined) {
+  $('body').append('<p>"$" is undefined</p>');
+} else {
+  $('body').append('<p>"$" exists!</p>');
+}
