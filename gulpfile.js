179aa7d9d47a974b5988647db097c8acb8a8788a
Josh Kramer <jkjustjoshing@gmail.com>
2016-11-13 15:35:38 -0500

    Set up ESLint
    ESLint will show you common issues with your code to help you maintain best practices. There are a LOT of options, so I recommend you just use my .eslintrc file for now. If you want to dig into it later please do (it's very well commented)! It's just not in the scope of this class.
    
    > npm install gulp-eslint --save-dev
    
    Let's create a gulp task called "lint" that runs eslint on all javascript in our app folder and shows any errors.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
   gulp.src('./app/**/*.js')
     .pipe(gulp.dest('build'));
+});
+
+gulp.task('lint', function () {
+  gulp.src('./app/**/*.js')
+    // Run linter and save results on the eslint object (doesn't output)
+    .pipe(eslint())
+    // Take the output and show it in the console
+    .pipe(eslint.format())
+    // If there are any errors, stop right here instead of continuing
+    .pipe(eslint.failAfterError());
 });
